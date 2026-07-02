7865d5ab17ff20fa59cb066a21459770f8e1c6f5
maisiwei <maisiwei@gmail.com>
2026-07-03 07:55:45 +0900

    Fix: Add UTF-8 string encoding support to qrcode.js to resolve loop stopping on multibyte text

--- build_web_app.py
+++ build_web_app.py
@@ -591,6 +591,26 @@ def main():
         playFrames();
     }}
 
+    function toUTF8String(str) {{
+        var out, i, len, c;
+        out = "";
+        len = str.length;
+        for(i = 0; i < len; i++) {{
+            c = str.charCodeAt(i);
+            if ((c >= 0x0001) && (c <= 0x007F)) {{
+                out += str.charAt(i);
+            }} else if (c > 0x07FF) {{
+                out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+                out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+                out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+            }} else {{
+                out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+                out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+            }}
+        }}
+        return out;
+    }}
+
     function playFrames() {{
         if (sendTimer) clearInterval(sendTimer);
         
@@ -602,7 +622,7 @@ def main():
             var headerText = (currentFrameIndex + 1) + "/" + total + ":" + payload;
             
             qrGenerator.clear();
-            qrGenerator.makeCode(headerText);
+            qrGenerator.makeCode(toUTF8String(headerText));
             
             document.getElementById('frame-indicator').innerText = (currentFrameIndex + 1) + " / " + total;
             
